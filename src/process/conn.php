502b5c724f50ee4677c51a7cc6b4e8a86b88adbb
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2023-04-30 01:47:25 -0300

    trabalho

--- src/process/conn.php
+++ src/process/conn.php
@@ -1,25 +1,30 @@
 <?php
 
+  
   session_start();
 
   $user = "root";
   $pass = "";
   $db = "livro_receita";
   $host = "localhost";
+  global $conn;
 
   try {
 
     $conn = new PDO("mysql:host={$host};dbname={$db}", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    echo "<h1>Deu certo</h1>";
 
   } catch (PDOException $e) {
 
     print "Erro: " . $e->getMessage() . "<br/>";
     die();
-    echo "Deu erro";
 
   }
+
+    // $stmt = $conn->prepare("SELECT * FROM tabela_teste");
+    // $stmt->execute();
+    // $dados = $stmt->fetchAll();
+
 
 ?>
